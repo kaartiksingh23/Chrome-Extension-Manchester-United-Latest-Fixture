f38ec869175a0fa5be2c8ff1cf95f9ecbedca6e2
kaartiksingh23 <kaartiksingh23@gmail.com>
2020-03-05 18:18:27 +0530

    renders the latest mufc fixture

---
+++ src/popup.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function(){
+    var getDetailsButton = document.getElementById('getDetailsButton');
+    
+    var xhr = new XMLHttpRequest();
+
+    
+    xhr.open('GET','https://www.google.com/search?q=manchester+united', true);
+    xhr.send();
+
+    function processRequest(e){
+    
+        if(xhr.readyState == 4 && xhr.status == 200){
+            
+            var detailElement = document.createElement('html');
+            detailElement.innerHTML = xhr.responseText;
+            
+            let data = detailElement.getElementsByClassName('imso_mh__tm-scr imso_mh__mh-bd imso-hov imso_mh__nma')[0];
+            let nodesToRemove = data.getElementsByClassName("imso_mh__t-l-cont kno-fb-ctx")
+
+            while(nodesToRemove.length>0){
+                nodesToRemove[0].parentNode.removeChild(nodesToRemove[0]);
+            }
+
+            document.getElementById("details").innerHTML = data.outerHTML;
+            
+        }
+    }
+
+    xhr.addEventListener("readystatechange",processRequest,false);
+    
+}, false);
+
+   
+
+    /* 
+
+    Value	State	            Description
+    0	    UNSENT	            The open method hasn't been called yet
+    1	    OPENED	            The send method has been called
+    2	    HEADERS_RECEIVED	The send method has been called and the HTTP request has returned the status and headers
+    3	    LOADING	            The HTTP request response is being downloaded
+    4	    DONE	            Everything has completed
+
+    */
+
+
+    
